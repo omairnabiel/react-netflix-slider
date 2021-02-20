4ad00393255e699de55df88cb748dbe35650393a
Omair Nabiel <nabiel.omair@gmail.com>
2021-02-20 19:44:04 +0500

    add tests for searchInput component

--- wookie-movie-app/components/search-input/__tests__/SearchInput.spec.tsx
+++ wookie-movie-app/components/search-input/__tests__/SearchInput.spec.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import {render, RenderResult,fireEvent} from "@testing-library/react"
 import SearchInput from "../SearchInput"
+import { MovieService } from "../../../service/movie.service"
 
 // mocking recoiljs hooks
 jest.mock('recoil', () => ({
@@ -9,13 +10,34 @@ jest.mock('recoil', () => ({
 
 }));
 
-
+// mock service functions
+jest.mock("../../../service/movie.service").fn(() => ({
+    get: ()=>{}
+}))
 
 
 let document: RenderResult
 describe("Search Input", () => {
-    it("placeholder value must be Search Movies", ()=> {
+    beforeEach(() => {
         document = render(<SearchInput />);
-        expect(document.getByPlaceholderText('Search Movies')).toBeInTheDocument();
     })
+
+    it("placeholder value must be Search Moviess", ()=> {
+        expect(document.getByPlaceholderText("Search Movies")).toBeTruthy()
+    })
+
+    it("should update search input value when typed", () => {
+        const input = document.getByPlaceholderText("Search Movies") as HTMLInputElement
+        fireEvent.change(input, {target:{value: "the dark knight"}})
+        expect(input.value).toEqual("the dark knight")
+    })
+
+    it("should call MovieService.get when input value is changed", async() => {
+        const input = document.getByPlaceholderText("Search Movies")
+        const getMoviesSpy = spyOn(MovieService,"get")
+        fireEvent.change(input, {target:{value: "t"}})
+        await new Promise((resolve) => setTimeout(() => resolve(1),1000))
+       expect(getMoviesSpy).toHaveBeenCalled()
+    })
+
 })
